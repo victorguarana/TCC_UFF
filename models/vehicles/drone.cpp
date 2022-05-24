643d3cf189d303bcfcaebd731e469b944e527e82
victorguarana <victorguarana@hotmail.com>
2022-05-23 23:24:54 -0300

    fix drones struct and takeoff

--- models/vehicles/drone.cpp
+++ models/vehicles/drone.cpp
@@ -10,8 +10,7 @@
 #define DEFAULT_DRONE_SPEED 25
 
 struct Flight{
-    Point initial_point;
-    Point returning_point;
+    int initial_point_index;
     vector<Point> route;
 };
 
@@ -47,33 +46,32 @@ class Drone : public Vehicle {
         bool isFlying(){
             return m_flying;
         }
-        void takeOff(Point t_takeoff_point){
+        void takeOff(Point t_takeoff_point, int t_route_index){
             m_flying = true;
             Flight flight;
-            flight.initial_point = t_takeoff_point;
+            flight.initial_point_index = t_route_index;
             flight.route.push_back(t_takeoff_point);
             m_flights.push_back(flight);
         }
         void land(Point t_land_point){
-            Flight flight = m_flights.at(m_flights.size()-1);
-            flight.route.push_back(t_land_point);
-            flight.returning_point = t_land_point;
+            Flight *flight = &m_flights.at(m_flights.size()-1);
+            flight->route.push_back(t_land_point);
             resetRange();
             resetStorage();
             m_flying = false;
         }
 
         void addPointToFlight(Point t_point){
-            Flight flight = m_flights.at(m_flights.size()-1);
-            flight.route.push_back(t_point);
+            Flight *flight = &m_flights.at(m_flights.size()-1);
+            flight->route.push_back(t_point);
         }
 
         double route_distance(){
             double total_distance = 0;
             for(int i = 0; i < m_flights.size(); i++){
                 Flight flight = m_flights.at(i);
-                for(int j = 0; i < flight.route.size()-1; i++){
-                    total_distance += Point::distanceBetweenPoints(flight.route.at(i), flight.route.at(i+1));
+                for(int j = 0; j < flight.route.size()-1; j++){
+                    total_distance += Point::distanceBetweenPoints(flight.route.at(j), flight.route.at(j+1));
                 }
             }
             return total_distance;
