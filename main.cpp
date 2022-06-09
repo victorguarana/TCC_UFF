d1f2ddfb38f8a4792abf8dc2900c0117a9cd96b9
victorguarana <victorguarana@hotmail.com>
2022-06-08 21:31:34 -0300

    update main file

--- main.cpp
+++ main.cpp
@@ -1,40 +1,10 @@
-/**
-#include <iostream>
-#include "models/vehicles/drone.cpp"
-#include "models/vehicles/car.cpp"
-#include "models/point.cpp"
-#include "models/car_stop.cpp"
-
-using namespace std;
-
-int main(){
-    Drone drone("Drone");
-    Drone drone2(drone);
-    drone2.deliver(10, 20);
-    Drone* p_drone2 = &drone2;
-    p_drone2->deliver(0, 20);
-    drone2.resetAttributes();
-
-    Point cliente0("nome", 10, 11, 10);
-
-    CarStop carstop(&cliente0);
-
-    // cout << car.getRemainingStorage() << endl;
-    // cout << drone.getRemainingStorage() << endl;
-    // cout << car2.getRemainingStorage() << endl;
-};
-/*/
-// #include "helpers/database.cpp"
 #include "models/point.cpp"
 #include "models/map.cpp"
 #include "heusristics/greedy.cpp"
 #include "heusristics/ils.cpp"
-// #include "measurer.cpp"
 
 
 int main(){
-    // Database database_instane;
-    
     Point deposit("Deposito", 0, 0);
 
     Drone drone("Drone 1");
@@ -88,4 +58,3 @@ int main(){
     cout << "\n" << endl;
     return 0; 
 }
-/**/
